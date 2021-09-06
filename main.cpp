858c70a482559527a54eda7f7783b18acf18c116
Steffen Häfelinger <67802450+shaefelinger@users.noreply.github.com>
2021-09-06 19:41:09 +0000

    fixed some text in the comments

--- main.cpp
+++ main.cpp
@@ -77,7 +77,7 @@ returning lhs
 good to go!
 */
 
-/*clea  
+/*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
